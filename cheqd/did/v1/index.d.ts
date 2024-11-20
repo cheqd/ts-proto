2892c2109c0e821f25d30ded947fa842c54daa5d
Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>
2024-11-20 18:17:55 +0200

    chore: Removed deprecated references

--- cheqd/did/v1/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from './common';
-export * from './did';
-export * from './query';
-export * from './genesis';
-export * from './tx';
-export * from './stateValue';
-export declare const protobufPackage = "cheqdid.cheqdnode.cheqd.v1";
-//# sourceMappingURL=index.d.ts.map
--- cheqd/resource/v1/index.js
+++
@@ -1,6 +0,0 @@
-export * from './genesis';
-export * from './query';
-export * from './resource';
-export * from './tx';
-export const protobufPackage = "cheqdid.cheqdnode.resource.v1";
-//# sourceMappingURL=index.js.map
