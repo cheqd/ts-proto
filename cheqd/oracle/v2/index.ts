81a1d14870353656be5a8207ec01b7ad0bb5093c
Tasos Derisiotis <tasosderiziotis@gmail.com>
2025-11-17 17:38:07 +0200

    build: Added all-encompassing exports

---
+++ cheqd/oracle/v2/index.ts
@@ -0,0 +1,7 @@
+export * from './abci.js';
+export * from './events.js';
+export * from './genesis.js';
+export * from './oracle.js';
+export * from './query.js';
+export * from './tx.js';
+export const protobufPackage = 'cheqd.oracle.v2';
