b1461aad9fa396ac816425fb124324ce4f751e92
Tasos Derisiotis <tasosderiziotis@gmail.com>
2025-11-20 14:25:35 +0200

    build: Final exports

---
+++ cheqd/oracle/v2/index.js
@@ -0,0 +1,8 @@
+export * from './abci.js';
+export * from './events.js';
+export * from './genesis.js';
+export * from './oracle.js';
+export * from './query.js';
+export * from './tx.js';
+export const protobufPackage = 'cheqd.oracle.v2';
+//# sourceMappingURL=index.js.map
